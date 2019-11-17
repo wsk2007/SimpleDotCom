bc4bb6796cbcd598f600ac37ce5b1728a386c151
BuildTools <unconfigured@null.spigotmc.org>
2019-11-17 16:41:56 +0800

    create classes

---
+++ SimpleDotCom/src/org/wsk/simpledotcom/SimpleDotCom.java
@@ -0,0 +1,5 @@
+package org.wsk.simpledotcom;
+
+public class SimpleDotCom {
+
+}
---
+++ SimpleDotCom/src/org/wsk/simpledotcom/SimpleDotComGame.java
@@ -0,0 +1,10 @@
+package org.wsk.simpledotcom;
+
+public class SimpleDotComGame {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
