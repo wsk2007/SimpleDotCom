853c4e33268288ee721abb3a9634175825f440fc
BuildTools <unconfigured@null.spigotmc.org>
2019-11-17 18:29:54 +0800

    finish

---
+++ SimpleDotCom/src/org/wsk/simpledotcom/GameHelper.java
@@ -0,0 +1,22 @@
+package org.wsk.simpledotcom;
+import java.io.*;
+public class GameHelper {
+
+	public String getUserInput(String prompt) {
+		String inputLine = null;
+		System.out.println(prompt + "   ");
+		try {
+			BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+			inputLine = is.readLine();
+			if (inputLine.length() == 0) {
+				return null;
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return inputLine;
+	}
+
+
+
+}
